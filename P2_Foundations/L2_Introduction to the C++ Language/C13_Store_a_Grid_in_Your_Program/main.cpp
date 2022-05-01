f3d9274e402fe2b5edc9ca7c071b56c32a04f740
Ibrahim Abdelhamed <i_abdulhamid@outlook.com>
2022-05-01 23:36:49 +0200

    store a grid quiz solution

---
+++ P2_Foundations/L2_Introduction to the C++ Language/C13_Store_a_Grid_in_Your_Program/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    // TODO: Declare a "board" variable here, and store the data provided above.
+
+    std::vector<std::vector<int>> board = {{0, 1, 0, 0, 0, 0},
+                                           {0, 1, 0, 0, 0, 0},
+                                           {0, 1, 0, 0, 0, 0},
+                                           {0, 1, 0, 0, 0, 0},
+                                           {0, 0, 0, 0, 1, 0}};
+    std::cout << "Hello!"
+              << "\n";
+}
